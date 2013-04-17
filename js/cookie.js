c92f471079e000738e737c337a81d4f9fd533755
Wu Pangbo <i@wpb93.com>
2013-04-17 12:34:25 +0800

    fixed bugs for IE

--- js/cookie.js
+++ js/cookie.js
@@ -9,12 +9,10 @@
 }
 
 function readCookie(name) {
-    var nameEQ = name + "=";
     var ca = document.cookie.split(';');
     for (var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+        var c = ca[i].trim().split('=');
+        if (c[0] == name) return c[1] === undefined ? '' : c[1];
     }
     return null;
 }
